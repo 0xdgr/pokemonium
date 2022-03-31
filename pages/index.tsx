060af3c89408f9880ecd40de2a9dc720e3084df7
0xdgr <0xdgr@pm.me>
2022-03-31 22:14:33 +0100

    feat: add pokemon list on home page with getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,59 @@
-import type { NextPage } from 'next';
-import { Button } from '@nextui-org/react';
+import type { GetStaticProps, NextPage } from 'next';
+
+import { Card, Grid, Image, Row, Text } from '@nextui-org/react';
+
+import { pokeApi } from '../api';
+import { PokemonListResponse, SmallPokemon } from '../interfaces';
 
 
-const Home: NextPage = () => {
+interface Props {
+    pokemons: SmallPokemon[];
+}
+
+const Home: NextPage<Props> = ({pokemons}) => {
     return (
-        <>
-            <Button color={'secondary'}>Click me</Button>
-        </>
+        <section>
+            <Grid.Container gap={2} justify="flex-start">
+                {
+                    pokemons.map(({name, id, image}) => (
+                        <Grid key={id} xs={6} sm={3} md={2} xl={1}>
+
+                            <Card hoverable clickable>
+                                <Card.Body css={{p: 1}}>
+                                    <Card.Image src={image} width="100%" height={140}/>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <Row justify="space-between">
+                                        <Text transform='capitalize'>{name}</Text>
+                                        <Text>#{id}</Text>
+                                    </Row>
+                                </Card.Footer>
+                            </Card>
+                        </Grid>
+                    ))
+                }
+            </Grid.Container>
+        </section>
     );
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+
+    const {data} = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151');
+
+    const pokemons: SmallPokemon[] = data.results.map((poke, i) => {
+        return {
+            ...poke,
+            id: i + 1,
+            image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i + 1}.png`
+        };
+    });
+
+    return {
+        props: {
+            pokemons
+        }
+    };
 }
 
 export default Home;
