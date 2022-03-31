68c8b82244d8f44d754b0e383f0f065735f49597
0xdgr <0xdgr@pm.me>
2022-03-31 22:14:58 +0100

    refactor: move pokemon list to component

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Card, Grid, Image, Row, Text } from '@nextui-org/react';
 
 import { pokeApi } from '../api';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
+import { PokeCard } from '../components/ui/PokeCard';
 
 
 interface Props {
@@ -16,20 +17,7 @@ const Home: NextPage<Props> = ({pokemons}) => {
             <Grid.Container gap={2} justify="flex-start">
                 {
                     pokemons.map(({name, id, image}) => (
-                        <Grid key={id} xs={6} sm={3} md={2} xl={1}>
-
-                            <Card hoverable clickable>
-                                <Card.Body css={{p: 1}}>
-                                    <Card.Image src={image} width="100%" height={140}/>
-                                </Card.Body>
-                                <Card.Footer>
-                                    <Row justify="space-between">
-                                        <Text transform='capitalize'>{name}</Text>
-                                        <Text>#{id}</Text>
-                                    </Row>
-                                </Card.Footer>
-                            </Card>
-                        </Grid>
+                        <PokeCard key={id} name={name} url={''} id={id} image={image}/>
                     ))
                 }
             </Grid.Container>
