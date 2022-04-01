c28e7fa8c6cdc4bd263653fb9647c71534abed0a
0xdgr <0xdgr@pm.me>
2022-04-01 15:25:08 +0100

    add: show sprites on nice layout for individual Pokémon page

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,37 +1,90 @@
-import { GetStaticProps, NextPage } from 'next';
+import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
-import { SmallPokemon } from '../../interfaces';
+import { PokemonResponse } from '../../interfaces';
 import { getAllPokemonsIds } from '../../lib/pokemons';
+import { pokeApi } from '../../api';
+import { Button, Card, Container, Grid, Image, Text } from '@nextui-org/react';
+import { HeartIcon } from '../../components/ui/HeartIcon';
 
 
 interface PokemonProps {
-    pokemon?: SmallPokemon;
-    id: string;
+    pokemon: PokemonResponse;
 }
 
-const PokemonPage: NextPage<PokemonProps> = ({id, pokemon}) => {
+const PokemonPage: NextPage<PokemonProps> = ({pokemon}) => {
 
     return (
-        <>
-            <p>{id}</p>
-        </>
+        <Grid.Container css={{marginTop: '5px'}} gap={2}>
+            <Grid xs={12} sm={4}>
+                <Card hoverable css={{padding: '30px'}}>
+                    <Card.Body>
+                        <Image src={pokemon.sprites.other?.home.front_default || ''}
+                               alt={pokemon.name}
+                               width="100%"
+                               height={200}
+                        />
+                    </Card.Body>
+                </Card>
+            </Grid>
+
+            <Grid xs={12} sm={8}>
+                <Card>
+                    <Card.Header css={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Text h1 transform="capitalize" css={{letterSpacing: '0.5px'}}>{pokemon.name}</Text>
+
+                        <Button auto color="error" icon={<HeartIcon filled={false}/>}>
+                        </Button>
+                    </Card.Header>
+
+                    <Card.Body>
+                        <Text size={30}>Game Sprites</Text>
+
+                        <Container display="flex">
+
+                            <Image src={pokemon.sprites.front_default}
+                                   alt="{pokemon.name} sprite front default"
+                                   width={100}
+                                   height={100}/>
+                            <Image src={pokemon.sprites.back_default}
+                                   alt="{pokemon.name} sprite back default"
+                                   width={100}
+                                   height={100}/>
+                            <Image src={pokemon.sprites.front_shiny}
+                                   alt="{pokemon.name} front shiny"
+                                   width={100}
+                                   height={100}/>
+                            <Image src={pokemon.sprites.back_shiny}
+                                   alt="{pokemon.name} sprite back shiny"
+                                   width={100}
+                                   height={100}/>
+
+                        </Container>
+                    </Card.Body>
+                </Card>
+            </Grid>
+        </Grid.Container>
     );
 }
 
-export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     const paths = getAllPokemonsIds();
 
     return {
         paths,
-        fallback: true
+        fallback: false
     };
 }
 
 export const getStaticProps: GetStaticProps = async ({params}) => {
 
+    const {id} = params as { id: string };
+
+    const {data} = await pokeApi.get<PokemonResponse>(`/pokemon/${id}`);
+
     return {
         props: {
-            id: params.id
+            id,
+            pokemon: data
         },
     }
 }
