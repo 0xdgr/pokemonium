540afdbe90a6c4a6e18ab5f90ea9495d6e30a949
0xdgr <0xdgr@pm.me>
2022-04-04 14:07:55 +0100

    feat: save and load favorite state

--- components/ui/SaveFavoriteButton.tsx
+++ components/ui/SaveFavoriteButton.tsx
@@ -1,22 +1,31 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import { Button } from '@nextui-org/react';
 
 import { HeartIcon } from './HeartIcon';
+import { toggleFavorite } from '../../lib/favorites';
+import { isFavorited } from '../../lib/favorites/favoritesStorage';
 
 
 interface Props {
     id: number;
 }
 
-export const SaveFavoriteButton: FC<Props> = (id) => {
+export const SaveFavoriteButton: FC<Props> = ({id}) => {
+
+    const [filled, setFilled] = useState<boolean>(false);
+
+    useEffect(() => {
+         setFilled(isFavorited(id));
+    }, [id, filled]);
 
     const onToggleFavorite = () => {
-        console.log(id)
+        toggleFavorite(id);
+        setFilled(!filled);
     }
 
     return (
-        <Button auto color="error" icon={<HeartIcon filled={false}/>}
+        <Button auto color="error" icon={<HeartIcon filled={filled}/>}
                 onClick={onToggleFavorite}>
         </Button>
     );
